f0afdd68dc246f839ba1f6272bbb2d81bc9a8dae
Benjamin <benjaminking1337@live.com>
2022-05-03 22:31:44 +0200

    base nav with styling done

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -4,24 +4,30 @@ import css from "./index.module.scss";
 
 const Nav: NextPage = () => {
   return (
-    
     <div>
       <div className={css.nav}>
-        <Link href="/">
-          <a>
-            <h1>LOGO</h1>
-          </a>
-        </Link>
-        <Link href="/register">
-          <a>
-            <h2>Register</h2>
-          </a>
-        </Link>
-        <Link href="/login">
-          <a>
-            <h2>Login</h2>
-          </a>
-        </Link>
+        <div className={css.logo}>
+          <div className={css.logoWrapper}>
+            <img className={css.logo1} src="/logo.svg" alt="logo" />
+          </div>
+          <Link href="/">
+            <a>
+              <h1>Kikaku</h1>
+            </a>
+          </Link>
+        </div>
+        <div className={css.menu}>
+          <Link href="/register">
+            <a>
+              <h2>Register</h2>
+            </a>
+          </Link>
+          <Link href="/login">
+            <a>
+              <h2>Login</h2>
+            </a>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,19 @@ import css from './index.module.scss'
 const Home: NextPage = () => {
   return (
 
-    <div>
-      /homepage
+    <div className={css.homePageWrapper}>
+      <div className={css.contentWrapper}>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div className={css.logoWrapper}>
+            <img className={css.logo} src="/logo.svg" alt="logo" />
+          </div>
+          <div>
+            <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Welcome to Kikaku!</h1>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
