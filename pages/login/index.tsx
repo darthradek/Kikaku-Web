c5721b7ce962b6f9b0f8cf6c8bd3e48c03b546a2
darthradek <radoslawhaller@gmail.com>
2022-05-03 16:07:31 +0200

    creating the project

---
+++ pages/login/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import css from './index.module.css'
+
+const LoginPage: NextPage = () => {
+    return (
+        <div className={css.color}>
+            add tailwind
+        </div>
+    )
+}
+
+export default LoginPage
