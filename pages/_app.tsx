4a2e4ec91065f79a9faa1636bf5d7bf9fc83ee92
Benjamin <benjaminking1337@live.com>
2022-05-07 20:32:26 +0200

    sidebar attempt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/navbar/index";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import SimpleSidebar from "../components/sidebar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Navbar />
+      <SimpleSidebar />
 
       <Component {...pageProps} />
     </>
