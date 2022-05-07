c2731afae46de0c93e73336296b367fd697de99d
darthradek <radoslawhaller@gmail.com>
2022-05-07 21:10:01 +0200

    Big changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "../components/navbar/index";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import Sidebar from "../components/sidebar/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
