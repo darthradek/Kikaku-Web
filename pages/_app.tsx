b786955cfdfdba677e5c006a65a636fc0853cb77
Benjamin <benjaminking1337@live.com>
2022-05-07 22:01:26 +0200

    milos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Hero from "../ui/components/hero/index"
+import Carousel from "../ui/components/carousel/index"
 import Navbar from "../ui/components/navbar/index"
 import Navbar2 from "../ui/components/navbarv2/index"
 
@@ -8,9 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider>
-        //navbar tests
+        {/* navbar tests */}
         <Navbar />
         <Navbar2 />
+        <Carousel />
+        <Hero />
         <Component {...pageProps} />
       </ChakraProvider>
     </>
---
+++ ui/components/carousel/index.tsx
@@ -0,0 +1,102 @@
+import type { NextPage } from "next";
+// import Link from "next/link";   
+import css from "./index.module.scss";
+
+import React from 'react';
+import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+
+// Settings for the slider
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+export default function Carousel() {
+  // As we have used custom buttons, we need a reference variable to
+  // change the state
+  const [slider, setSlider] = React.useState<Slider | null>(null);
+
+  // These are the breakpoints which changes the position of the
+  // buttons as the screen size changes
+  const top = useBreakpointValue({ base: '90%', md: '50%' });
+  const side = useBreakpointValue({ base: '30%', md: '10px' });
+
+  // These are the images used in the slide
+  const cards = [
+    'https://steamuserimages-a.akamaihd.net/ugc/995764246294320390/5E51E3F97ACB5D0419C65329BA942434BF81DA6E/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
+    'https://i.kym-cdn.com/entries/icons/original/000/010/843/ricardo.jpg',
+    'https://i.pinimg.com/originals/a4/0b/01/a40b01172b2f751fef7bcd2294af9dfd.jpg'   
+  ];
+
+  return (
+    <Box
+      position={'relative'}
+      height={'600px'}
+      width={'full'}
+      overflow={'hidden'}>
+      {/* CSS files for react-slick */}
+      <link
+        rel="stylesheet"
+        type="text/css"
+        charSet="UTF-8"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+      />
+      {/* Left Icon */}
+      <IconButton
+        aria-label="left-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        left={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickPrev()}>
+        <BiLeftArrowAlt />
+      </IconButton>
+      {/* Right Icon */}
+      <IconButton
+        aria-label="right-arrow"
+        colorScheme="messenger"
+        borderRadius="full"
+        position="absolute"
+        right={side}
+        top={top}
+        transform={'translate(0%, -50%)'}
+        zIndex={2}
+        onClick={() => slider?.slickNext()}>
+        <BiRightArrowAlt />
+      </IconButton>
+      {/* Slider */}
+      <Slider {...settings} ref={(slider: any) => setSlider(slider)}>
+        {cards.map((url, index) => (
+          <Box
+            key={index}
+            height={'3xl'}
+            position="relative"
+            backgroundPosition="center"
+            backgroundRepeat="no-repeat"
+            backgroundSize="cover"
+            backgroundImage={`url(${url})`}
+          />
+        ))}
+      </Slider>
+    </Box>
+  );
+}
