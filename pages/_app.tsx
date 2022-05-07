57bcab9dc497d496dda23b2b7a101c23e425f42f
Benjamin <benjaminking1337@live.com>
2022-05-07 22:02:55 +0200

    zealous retribution

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,25 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import Hero from "../ui/components/hero/index"
-import Carousel from "../ui/components/carousel/index"
-import Navbar from "../ui/components/navbar/index"
-import Navbar2 from "../ui/components/navbarv2/index"
+
+{/* component tests */}
+import Hero from "../ui/components/hero/index";
+import Carousel from "../ui/components/carousel/index";
+import Navbar from "../ui/components/navbar/index";
+import Navbar2 from "../ui/components/navbarv2/index";
+{/* component tests */}
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider>
-        {/* navbar tests */}
+        {/* component tests */}
         <Navbar />
         <Navbar2 />
         <Carousel />
         <Hero />
+        {/* component tests */}
         <Component {...pageProps} />
       </ChakraProvider>
     </>
