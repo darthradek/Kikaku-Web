6485938d1ff2e418d5657c16c71ea3cf79b3c874
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2022-05-07 22:10:01 +0200

    Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import css from "./index.module.scss";
+import { Heading, Box, Text, Button, Center } from '@chakra-ui/react'
 
 const Home: NextPage = () => {
-  return <div className={css.homePageWrapper}>homepage</div>;
+  return <div>
+    <Center h='100vh' background='#E1E8EB'>
+      <Box maxW='32rem'>
+        <Center><Heading mb={4}>Ready to dive in?</Heading></Center>
+        <Text fontSize='xl' align='center'>
+          Create your account or log in to start planning your projects
+        </Text>
+        <Center>
+          <Link href="/register">
+            <Button size='lg' colorScheme='gray' mt='24px' mr='12px'>Sign In</Button>
+          </Link>
+          <Link href="/login">
+            <Button size='lg' colorScheme='blue' mt='24px'>Log in</Button>
+          </Link>
+        </Center>
+      </Box>
+    </Center>
+  </div>;
 };
 
 export default Home;
