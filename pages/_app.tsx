e186114de109aedb4e5ad5506b5b6305a9e0eb3a
Benjamin <benjaminking1337@live.com>
2022-05-07 21:45:13 +0200

    some navbar components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Navbar from "../ui/components/navbar/index"
+import Navbar2 from "../ui/components/navbarv2/index"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ChakraProvider>
+        //navbar tests
+        <Navbar />
+        <Navbar2 />
         <Component {...pageProps} />
       </ChakraProvider>
     </>
